ea9d5e5e822c325b42bcaf495d543725f6c359c4
Requiem493 <akshaj.illa@gmail.com>
2024-02-21 19:41:29 -0500

    Added choice for dog or cat

---
+++ AnimalPicker.java
@@ -0,0 +1,16 @@
+package AnimalPicker;
+
+import java.util.Scanner;
+
+public class AnimalPicker {
+    public static void main(String [] args){
+        Scanner scan = new Scanner(System.in);
+        String animalChoice = "";
+        System.out.println("What animal would you like to see?");
+        System.out.println("Dog or Cat?");
+        animalChoice = scan.nextLine();
+        if (animalChoice.toUpperCase().equals("dog")){
+            
+        }
+    }
+}
