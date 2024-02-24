e36d99be85910c8b7d0b89077845060c3656ce07
Requiem493 <akshaj.illa@gmail.com>
2024-02-24 11:39:12 -0500

    added options for a dog and cat with error trapping

--- AnimalPicker.java
+++ AnimalPicker.java
@@ -9,8 +9,15 @@ public class AnimalPicker {
         System.out.println("What animal would you like to see?");
         System.out.println("Dog or Cat?");
         animalChoice = scan.nextLine();
+        while(!animalChoice.toUpperCase().equals("dog")|| !animalChoice.toUpperCase().equals("cat")){
+            System.out.println("What animal would you like to see?");
+            System.out.println("Dog or Cat?");
+        }
         if (animalChoice.toUpperCase().equals("dog")){
-            
+            System.out.println("          __\n \\ ______/ V`-,\n  }        /~~\n /_)^ --,r\'\n|b      |b");
+
+        }else if(animalChoice.toUpperCase().equals("cat")){
+            System.out.println(" _._     _,-\'\"\"`-._\n(,-.`._,\'(       |\\`-/|\n    `-.-\' \\ )-`( , o o)\n          `-    \\`_`\"\'-");
         }
     }
 }
