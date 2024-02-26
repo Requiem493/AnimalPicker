d3b7ac335d56af08af811e50198c06b2aafb8aaa
Requiem493 <akshaj.illa@gmail.com>
2024-02-26 15:08:37 -0500

    added selection for 3 animals

---
+++ AnimalPicker3.java
@@ -0,0 +1,12 @@
+package AnimalPicker;
+
+import java.util.Scanner;
+
+public class AnimalPicker3 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        String animalChoice = "";
+        System.out.println("What animal would you like to see?");
+        System.out.println("Dog, Cat or Fish?");
+    }
+}
