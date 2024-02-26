ecfb25954c0ec3d8ba30491fe292ef8592c6a792
Requiem493 <akshaj.illa@gmail.com>
2024-02-26 05:58:57 -0500

    why is it not working?

--- BranchAnimalPicker.java
+++ BranchAnimalPicker.java
@@ -7,7 +7,11 @@ public class BranchAnimalPicker {
         Scanner scan = new Scanner(System.in);
         String animalChoice = "";
         System.out.println("What animal would you like to see?");
-        System.out.println("Dog or Cat?");
-        
+        System.out.println("Dog, Cat or Fish?");
+        animalChoice = scan.nextLine();
+        while(!animalChoice.toUpperCase().equals("dog")|| !animalChoice.toUpperCase().equals("cat")||!animalChoice.toUpperCase().equals("fish")){
+            System.out.println("What animal would you like to see?");
+            System.out.println("Dog, Cat, or Fish?");
+        }
     }
 }
