0c08aef139fa1b91bfa8196566e2cc8b09a56657
Requiem493 <akshaj.illa@gmail.com>
2024-02-26 05:32:13 -0500

    added selection for dog or cat

--- BranchAnimalPicker.java
+++ BranchAnimalPicker.java
@@ -1,7 +1,13 @@
 package AnimalPicker;
 
+import java.util.Scanner;
+
 public class BranchAnimalPicker {
     public static void main(String[] args){
+        Scanner scan = new Scanner(System.in);
+        String animalChoice = "";
+        System.out.println("What animal would you like to see?");
+        System.out.println("Dog or Cat?");
         
     }
 }
